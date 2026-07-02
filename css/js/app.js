5cb6713ecde8dde0ff35cc5e21fced9d2b5faa95
shangyuanjiuzhi <shangyuanjiuzhi@126.com>
2026-07-02 18:21:22 +0800

    Delete css/js directory

--- css/js/app.js
+++
@@ -1,6 +0,0 @@
-// App Page JavaScript
-// Uses common initTabs function from common.js
-
-document.addEventListener('DOMContentLoaded', function() {
-  initTabs('#app-tabs');
-});
--- css/js/transportation.js
+++
@@ -1,8 +0,0 @@
-// Transportation Page JavaScript
-// Uses common initTabs function from common.js
-
-document.addEventListener('DOMContentLoaded', function() {
-  initTabs('#transportationTabs', {
-    useHiddenClass: false
-  });
-});
